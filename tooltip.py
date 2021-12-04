4b2ea5ae3c045c75408168eccd99bfa5a81b6246
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2021-12-05 01:56:33 +0800

    Bug fix, add version number

--- tooltip.py
+++ tooltip.py
@@ -1,3 +1,5 @@
+# ToolTip widget for Python Tkinter V1.1
+
 from tkinter import *
 from time import time
 
@@ -10,11 +12,11 @@ class ToolTip(Toplevel):
           wdgt:    The widget this ToolTip is assigned to
           msg:     A static string message assigned to the ToolTip
           msgFunc: A function that retrieves a string to use as the ToolTip text
-          delay:   显示提示框前需要等待的时间(delay + 1秒)，为了避免出现问题，请使用整数(注：设为0表示无等待时间，设为1表示等待2秒，以此类推)
+          delay:   Eg: 'delay = 2' means delayed for 3s
           follow:  If True, the ToolTip follows motion, otherwise hides
-          bgColor: 提示框背景色
-          font:    可设置字体样式和大小, 如font=('微软雅黑', size=8)
-          stime:   提示框显示时长(秒), 设为0表示不限时长
+          bgColor: ToolTip background color
+          font:    Eg: font=('Microsoft YaHei UI', size=8)
+          stime:   Eg: 'stime = 3' means hide the tip after 4s
         """
         self.wdgt = wdgt
         self.parent = self.wdgt.master                                          # The parent of the ToolTip is the parent of the ToolTips widget
@@ -30,11 +32,13 @@ class ToolTip(Toplevel):
         self.delay = delay
         self.follow = follow
         self.stime = stime
+        self.timer_id = None
         self.visible = 0
         self.lastMotion = 0
         Message(self, textvariable=self.msgVar, bg=bgColor, aspect=1000, font=font, bd=-1).grid() # The text of the ToolTip is displayed in a Message widget
         self.wdgt.bind('<Enter>', self.spawn, '+')                                                # Add bindings to the widget.This will NOT override bindings that the widget already has
         self.wdgt.bind('<Leave>', self.hide, '+')
+        self.wdgt.bind('<Button-1>', self.hide, '+')
         self.wdgt.bind('<Motion>', self.move, '+')
 
     def spawn(self, event=None):
@@ -47,8 +51,8 @@ class ToolTip(Toplevel):
         self.visible = 1
         if self.delay == 0:
             self.show()
-        else:
-            self.after(int((self.delay + 1) * 1000), self.show)                 # The after function takes a time argument in miliseconds
+        elif self.timer_id is None:
+            self.timer_id = self.after(int((self.delay + 1) * 1000), self.show) # The after function takes a time argument in miliseconds
 
     def show(self):
         #Displays the ToolTip if the time delay has been long enough
@@ -57,7 +61,7 @@ class ToolTip(Toplevel):
         if self.visible == 2:
             self.deiconify()
         if self.stime != 0:
-            self.after(int(self.stime * 1000), self.hide)
+            self.timer_id = self.after(int((self.stime + 1) * 1000), self.hide)
 
     def move(self, event):
         #Processes motion within the widget
@@ -66,17 +70,16 @@ class ToolTip(Toplevel):
           event: The event that called this function
         """
         self.lastMotion = time()
-        if self.follow == False and self.delay != 0:                            # If the follow flag is not set, motion within the widget will make the ToolTip dissapear(Unless delay set to 0)
+        if self.follow == False and self.delay != 0 and self.stime == 0:        # If the follow flag is not set, motion within the widget will make the ToolTip disappear(Unless delay set to 0 or stime not 0)
             self.withdraw()
             self.visible = 1
         self.geometry('+%i+%i' % (event.x_root, event.y_root+20))               # Offset the ToolTip 20 pixes south of the pointer
         try:
-            self.msgVar.set(self.msgFunc())                                     # Try to call the message function.Will not change the message if the message function is None or the message function fails
+            if self.msgFunc is not None:
+                self.msgVar.set(eval(self.msgFunc))                             # Try to call the message function.Will not change the message if the message function is None or the message function fails
         except:
             pass
-        if self.delay == 0:
-            self.show()
-        else:
+        if self.delay != 0:
             self.after(int((self.delay + 1) * 1000), self.show)
 
     def hide(self, event=None):
@@ -85,6 +88,10 @@ class ToolTip(Toplevel):
         Arguments:
           event: The event that called this function
         """
+        if self.timer_id is not None:
+            tid = self.timer_id
+            self.timer_id = None
+            self.after_cancel(tid)
         self.withdraw()
         self.visible = 0
 
@@ -126,7 +133,7 @@ def example():
             text += '-follow\n'
         if j % 2 == 0:
             msg = None
-            msgFunc = None
+            msgFunc = 'time()'
             text += 'Message Function'
         else:
             msg = 'Button at %s' % str((i,j))
